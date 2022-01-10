2dfc9e3d370b51cbdb4b40ee73a321f9bdfb897d
Bohdan-Dachkovsky <90333649+Bohdan-Dachkovsky@users.noreply.github.com>
2022-01-10 15:26:38 +0200

    Add number of the list

---
+++ js/task-01.js
@@ -0,0 +1,6 @@
+const numberOfElements = document.querySelectorAll(`#categories .item`);
+console.log(numberOfElements.length);
+
+const getHeader = document.querySelectorAll(`h2`)
+getHeader.classList.toggle(`new-class`);
+console.log(getHeader.innerHTML);
