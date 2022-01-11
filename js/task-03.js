fa70f707ace64c8842cfbe3840e095be898b84b5
Bohdan-Dachkovsky <90333649+Bohdan-Dachkovsky@users.noreply.github.com>
2022-01-11 15:00:02 +0200

    3 task

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,22 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+const newLi = document.createElement(`li`);
+const picture = document.createElement(`img`);
+picture.setAttribute(`url`, 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+picture.setAttribute(`alt`,'White and Black Long Fur Cat');
+newLi.insertAdjacentHTML("beforeBegin", picture);
+const elsePicture = document.createElement(`img`);
+elsePicture.setAttribute(`url`, 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+elsePicture.setAttribute(`alt`, 'Orange and White Koi Fish Near Yellow Koi Fish');
+newLi.insertAdjacentHTML("beforeBegin", elsePicture);
+const secondImg = document.createElement(`img`);
+secondImg.setAttribute(`url`, 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+secondImg.setAttribute(`alt`,'Group of Horses Running');
+newLi.insertAdjacentHTML("beforeBegin", secondImg);
+
+// const addImages = document.setAttributes(``)
+// addImages
+const setEl = document.querySelector(`.gallery`)
+setEl.insertAdjacentHTML("beforeBegin", newLi);
+
